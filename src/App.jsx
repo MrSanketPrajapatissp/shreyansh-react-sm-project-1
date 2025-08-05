bc0692ed5400d8891528b944f1afd1f5bb892dbf
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2025-08-05 17:02:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,69 @@
+import React from "react";
 import Card from "./components/Card";
-import "./App.css";
-import { useState } from "react";
-
 function App() {
-  const [banned, setBanned] = useState({ name: "John", isBanned: false });
+  const data = [
+    {
+      name: "John",
+      profession: "Developer",
+      image:
+        "https://media.istockphoto.com/id/1629252119/photo/teenage-boy-messaging-over-smart-phone-on-white-background.jpg?s=612x612&w=0&k=20&c=S-oC20Nr1jmtpV-vWPHUD2VNA9PUBNqEMpjIW-FcrgY=",
+      friends: false,
+    },
+    {
+      name: "Tripti",
+      profession: "Designer",
+      image:
+        "https://images.unsplash.com/photo-1514161955277-4ea47eef2ff9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGdpcmxzfGVufDB8fDB8fHww",
+      friends: false,
+    },
+    {
+      name: "Ananya",
+      profession: "Artist",
+      image:
+        "https://images.unsplash.com/photo-1627067227573-07bc616f46ee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGdpcmxzfGVufDB8fDB8fHww",
+      friends: true,
+    },
+    {
+      name: "Golu",
+      profession: "Coder",
+      image:
+        "https://images.unsplash.com/photo-1519456264917-42d0aa2e0625?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym95fGVufDB8fDB8fHww",
+      friends: false,
+    },
+  ];
+
+  const [realData, setRealData] = React.useState(data);
+  // state jaha banti vahi useye modify kiya ja sakta hai
+
+  const handleFriends = (changingIndex) => {
+    setRealData((prevdata) =>{
+      return prevdata.map((item,index) => {
+        if(changingIndex === index){
+          return {
+            ...item,
+            friends: !item.friends
+          }
+        }
+        return item;
+      })
+    })
+  }
 
   return (
-    <div className="px-4 py-2 ">
-      <div className="px-4 py-2"> {banned.name}</div>
-      <div className="px-4 py-2 ">{banned.isBanned.toString()}</div>
-
-      <button
-        onClick={() => setBanned({ ...banned, isBanned: !banned.isBanned })}
-        className={`${
-          banned.isBanned ? "bg-blue-500" : "bg-red-500"
-        } text-white px-2 py-1 rounded-full`}
-      >
-        Change
-      </button>
-    </div>
+    <>
+      <div className="w-full h-screen bg-zinc-200 flex justify-center items-center  gap-4">
+        {realData.map((item, index) => (
+          <div>
+            <Card
+              key={index}
+              index={index}
+              handleFriends={handleFriends}
+              values={item}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+function Card({ values, index,handleFriends }) {
+  const { name, profession, image, friends} = values;
 
-function Card() {
   return (
-    <div>
+    <div   className="w-52 bg-white rounded-md overflow-hidden">
      
+      <div className="w-full h-35 bg-blue-500">
+        <img src={image} className="w-full h-full object-left-top"></img>
+      </div>
+      <div className=" p-2">
+        <h1 className="text-2xl ">{name}</h1>
+        <p className="text-xs">{profession}</p>
+        <button
+          onClick={() => handleFriends(index)}
+         
+          className={`mt-2 ${friends ? "bg-green-500 hover:bg-green-600" : "bg-blue-500 hover:bg-blue-600"} text-white px-3 py-1 rounded `}
+        >
+          {friends=== true ? "friends": "Add Friend"}
+        </button>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
